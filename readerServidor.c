ad0770df21298ced54c2cbdb9122ae71e5a4d029
Paez911 <94703913+alejandro911pa@users.noreply.github.com>
2022-10-17 22:04:42 -0500

    añadi el editar y eliminar cliente

--- readerServidor.c
+++ readerServidor.c
@@ -8,111 +8,107 @@
 #include <libpq-fe.h>
 // Alejandro Paez Perez UNACH
 
-PGconn * conexion_db();
+PGconn *conexion_db();
 void comprobar_estadodb();
-void do_exit(PGconn * conexion) ;
+void do_exit(PGconn *conexion);
 
-static PGconn * conexion;
+static PGconn *conexion;
 
 void main()
 {
-    int fd, fd2, i;
+    
+    int fd, fd2, i, fdOpcion;
+    int filas,columnas;
     char salir;
+    int opcionhear;
+    char opcionf[10];
+    int opcioncase;
+    PGresult *resultado;
 
-   do    {
+    do
+    {
         char cadena[100];
         char cadena2[100];
 
-        //empieza a recibir el mensaje.
+        // empieza a recibir el mensaje.
         fd = open("FIFO1", O_RDONLY);
         read(fd, cadena, sizeof(cadena));
-        if (strcmp(cadena, "offline") == 0)
-        {
-            puts("\n\n\nUsuario 1 --offline--");
-            exit(-1);
-        }
-        printf("\ncliente hace: %s ", cadena);
-//empieza sentencia sql        
-        
+        printf("\nSentencia sql es:\n %s \n", cadena);
+        // empieza sentencia sql
         conexion = conexion_db();
-
         conexion = PQexec(conexion, cadena);
 
+/* 
+            resultado=PQexec(conexion,cadena);
+        printf("Filas:%d, Columnas:%d \n",PQntuples(resultado),PQnfields(resultado));
+        if(resultado != NULL){
+            printf("Imprimiendo los valores de la tabla Estudiante\n");
+            for (i = 0; i < PQntuples(resultado); i++){ //filas
+               for (int j = 0; j < PQnfields(resultado); j++){//columnas
+                    printf("%s\t",PQgetvalue(resultado,i,j));
+                        fd2 = open("FIFO2", O_WRONLY);
+                        write(fd2, filc, sizeof(filc));
+                        write(fd2, colc, sizeof(colc));
+                        close(fd2);
+                  }
+                  printf("\n");
+              }
+          }
+ */
         close(fd);
-
-
-
-        // proceso de mensaje enviado
-
+        // validacion si la inserccion es correcta
         if (PQstatus(conexion) != CONNECTION_OK)
         {
-            sprintf(cadena2, "Error de consulta");
-        }    else {
-            sprintf(cadena2, "Exitosa de consulta");
+            sprintf(cadena2, "Error de consulta valores invalido\n");
         }
-       
+        else
+        {
+            sprintf(cadena2, "Query exitoso\n");
+        }
+
         fd2 = open("FIFO2", O_WRONLY);
-       
-        
         write(fd2, cadena2, sizeof(cadena2));
         close(fd2);
 
-        if (strcmp(cadena2, "salir") == 0)
-        {
-            printf("\n\nSalir del programa [s/n]:  ");
-            scanf("%c", &salir);
-            if (salir == 's' || salir == 'S')
-            {
-                write(fd2, "offline", 13);
-                puts("\n\n\n--ofline--");
-                exit(-1);
-            }
-            else if (salir == 'n' || salir == 'N')
-            {
-                fflush(stdin);
-            }
-            else
-            {
-                fflush(stdin);
-                puts("\n\n\n\t-INVALIDO-");
-            }
-        }
-        
-
+        // limpiar la cadena para que no tires basura
         for (i = 0; i < 100; i++)
         {
             cadena[i] = '\0';
             cadena2[i] = '\0';
         }
-        
-   } while (1);
 
-   printf("SALIR\n");
-   return 0;
+    } while (1);
+
+    printf("SALIR\n");
+    
+    return 0;
 }
+ 
 
-PGconn * conexion_db() {
 
-    if (conexion == NULL) {
+// metodo conexion
+PGconn *conexion_db()
+{
+
+    if (conexion == NULL)
+    {
         conexion = PQconnectdb("host=127.0.0.1 port=5432 dbname=tienda user = postgres password=1234");
         printf("conexion exitosa\n");
     }
-
     comprobar_estadodb(conexion);
-
     return conexion;
 }
 
-void comprobar_estadodb() {
-
-    if (PQstatus (conexion) == CONNECTION_BAD) {
-        fprintf (stderr, "Falló la conexión a la base de datos:%s \n", PQerrorMessage (conexion));
+void comprobar_estadodb()
+{
+    if (PQstatus(conexion) == CONNECTION_BAD)
+    {
+        fprintf(stderr, "Falló la conexión a la base de datos:%s \n", PQerrorMessage(conexion));
     }
-
 }
 
-
-void do_exit(PGconn * conexion) {
+void do_exit(PGconn *conexion)
+{
     PQfinish(conexion);
     exit(1);
 }
--- writerCliente.c
+++ writerCliente.c
@@ -7,30 +7,31 @@
 #include <unistd.h>
 // Alejandro Paez Perez UNACH
 
-
-
 void menu_clientes();
 void inicio();
 void menu_facturas();
 void menu_articulos();
 void altacliente();
-
-    int fd, fd2, i;
-    char salir;
+void consultaClient();
+void eliminarCliente();
+void modifyCliente();
+int fd, fd2, fdOpcion, i;
+char salir;
 
 void main()
 {
-   inicio();
+    inicio();
     mkfifo("FIFO1", 0666);
     mkfifo("FIFO2", 0666);
+    mkfifo("fifopcion", 0666);
 }
 
-
-
-void inicio () {
+void inicio()
+{
 
     int opcion = 0;
-    do {
+    do
+    {
 
         printf("\n|-----------------|");
         printf("\n|    * Incio *    |");
@@ -43,33 +44,36 @@ void inicio () {
         printf("\n\n Escoja una opcion: ");
         scanf("%d", &opcion);
 
-        switch(opcion) {
-            case 1:
-                menu_clientes();
-                printf("clientes");
-                break;
-            case 2:
-                printf("Facturas");
-                menu_facturas();
-                break;
-            case 3:
-                menu_articulos();
-                printf("Articulos");
-                break;
-            case 4:
-                printf("Cerrado correctamente\n");
-                exit(-1);
-                break;    
-            default:
-                printf("\nOpcion no disponible\n");     break;
+        switch (opcion)
+        {
+        case 1:
+            menu_clientes();
+            printf("clientes");
+            break;
+        case 2:
+            printf("Facturas");
+            menu_facturas();
+            break;
+        case 3:
+            menu_articulos();
+            printf("Articulos");
+            break;
+        case 4:
+            printf("Cerrado correctamente\n");
+            exit(-1);
+            break;
+        default:
+            printf("\nOpcion no disponible\n");
+            break;
         }
-    }while (opcion != 4);
-
+    } while (opcion != 4);
 }
 
-void menu_clientes() {
+void menu_clientes()
+{
     int opcion = 0;
-    do {
+    do
+    {
         printf("\n|----------------------------------------------|");
         printf("\n|             * Submenu clientes *             |");
         printf("\n|----------------------------------------------|");
@@ -80,130 +84,419 @@ void menu_clientes() {
         printf("\n\n Escoja una opcion: ");
         scanf("%d", &opcion);
 
-        switch(opcion) {
-            case 1:
-                printf("menu alta cliente");
-                altacliente();
-                break;
-            case 2:
-                printf("consulta cliente");                       
-                break;
-            case 3:
-                printf("Eliminacion cliente");          
-                break;
-            case 4:
-                printf("modifcar cliente");
-                break;
-            case 5:
-                printf("mostrar tabla");        
-                break;                
-            case 6:
-                 inicio ();
-                 break;
-            default:
-                printf("\nOpcion no disponible\n");     break;
+        switch (opcion)
+        {
+        case 1:
+            printf("menu alta cliente");
+            altacliente();
+            break;
+        case 2:
+            printf("consulta cliente");
+            consultaClient();
+            break;
+        case 3:
+            printf("Eliminacion cliente");
+            eliminarCliente();
+            break;
+        case 4:
+            printf("modifcar cliente");
+            modifyCliente();
+            break;
+        case 5:
+            printf("mostrar tabla");
+            break;
+        case 6:
+            inicio();
+            break;
+        default:
+            printf("\nOpcion no disponible\n");
+            break;
         }
-    }while (opcion != 6);
+    } while (opcion != 6);
 }
 
-void altacliente(){
- char nombre[100];
- char apellido[100];
- char rfc[100];
- char direccion[100];
- char edad[100];
- char pais[100];
- char telefono[100];
- 
+void altacliente()
+{
+    char nombre[50];
+    char apellido[50];
+    char rfc[50];
+    char direccion[50];
+    char edad[10];
+    char pais[50];
+    char telefono[11];
+    char opcionf[3] = "1";
 
-        char cadena[100];
-        char cadena2[100];
-do    {
+    char cadena[100];
+    char cadena2[100];
+    do
+    {
 
         // se envia el mensaje.
         fflush(stdin);
         fd = open("FIFO1", O_WRONLY);
+        /*         /// para el menu del server
+                fdOpcion = open("fifopcion",O_WRONLY);
+                write(fdOpcion, opcionf, sizeof(opcionf));
+         */
 
         puts("\nescribe el nombre del cliente: ");
         printf("el nombre es:\n");
-        scanf("%s",&nombre);
+        scanf("%s", &nombre);
 
         printf("el apellido es:\n");
-        scanf("%s",&apellido);  
+        scanf("%s", &apellido);
 
         printf("el rfc es:\n");
-        scanf("%s",&rfc);
+        scanf("%s", &rfc);
 
         printf("el direccion es:\n");
-        scanf("%s",&direccion);
+        scanf("%s", &direccion);
 
         printf("el edad es:\n");
-        scanf("%s",&edad);
+        scanf("%s", &edad);
 
         printf("el pais es:\n");
-        scanf("%s",&pais);        
+        scanf("%s", &pais);
 
-       printf("el telefono es:\n");
-        scanf("%s",&telefono);        
-         sprintf( cadena,"insert into clientes values (DEFAULT, '%s', '%s', '%s', '%s', '%s', '%s', '%s');",nombre,apellido, rfc, direccion, edad, pais, telefono);
-      
+        printf("el telefono es:\n");
+        scanf("%s", &telefono);
 
-        if (strcmp(cadena, "salir") == 0)
-        {
-            printf("\n\nSalir del programa [s/n]:  ");
-            scanf("%c", &salir);
-            if (salir == 's' || salir == 'S')
-            {
-                write(fd, "offline", 13);
-                puts("\n\n\n--offline--");
-                exit(-1);
-            }
-            else if (salir == 'n' || salir == 'N')
-            {
-                fflush(stdin);
-            }
-            else
-            {
-                puts("\n\t\t-ERROR-");
-                fflush(stdin);
-            }
-        }
-        
+        sprintf(cadena, "insert into clientes values (DEFAULT, '%s', '%s', '%s', '%s', '%s', '%s', '%s');", nombre, apellido, rfc, direccion, edad, pais, telefono);
+        // mandamos la sentencia sql
         write(fd, cadena, sizeof(cadena));
         close(fd);
 
         // empieza a recibir el mensaje
         fd2 = open("FIFO2", O_RDONLY);
-        //lee lo que le manda el servidor
+        // lee lo que le manda el servidor
         read(fd2, cadena2, sizeof(cadena2));
-
-    /*     if (strcmp(cadena2, "offline") == 0)
-        {
-            puts("\n\n\nUsuario 2 --offline--");
-            exit(-1);
-        }
-      */   
-        //imprime lo que pasa
+        // imprime lo que pasa
         printf("\nServidor dice:  %s ", cadena2);
         close(fd2);
 
         for (i = 0; i < 100; i++)
         {
-            //reiniciar la cadena a 0
-            
+            // reiniciar la cadena a 0
             cadena[i] = '\0';
             cadena2[i] = '\0';
         }
-    fflush(stdin);
-    menu_clientes();
-    } while (1);        
+        fflush(stdin);
 
+        menu_clientes();
+    } while (1);
 }
 
+void consultaClient()
+{
 
-void menu_facturas() {
+    char cadena[100];
+    char cadena2[100];
+    char id_cliente[10];
+
+    do
+    {
+
+        // se envia el mensaje.
+        fflush(stdin);
+        fd = open("FIFO1", O_WRONLY);
+        printf("\n escribe el id del cliente que quieres mostrar:\n");
+        scanf("%s", &id_cliente);
+
+        sprintf(cadena, "SELECT * FROM clientes WHERE id_cliente = %s;", id_cliente);
+        write(fd, cadena, sizeof(cadena));
+        close(fd);
+
+        // empieza a recibir el mensaje
+        fd2 = open("FIFO2", O_RDONLY);
+        // lee lo que le manda el servidor
+        read(fd2, cadena2, sizeof(cadena2));
+
+        for (i = 0; i < 100; i++)
+        {
+            // reiniciar la cadena a 0
+            cadena[i] = '\0';
+            cadena2[i] = '\0';
+        }
+        fflush(stdin);
+        menu_clientes();
+
+    } while (1);
+}
+
+void eliminarCliente()
+{
+    char id_cliente[10];
+    char cadena[100];
+    char cadena2[100];
+    do
+    {
+        // se envia el mensaje.
+        fflush(stdin);
+        fd = open("FIFO1", O_WRONLY);
+        puts("\nescribe el ID del cliente a eliminar: ");
+        printf("el ID es:\n");
+        scanf("%s", &id_cliente);
+
+        sprintf(cadena, "DELETE FROM clientes WHERE id_cliente = %s", id_cliente);
+        // mandamos la sentencia sql
+        write(fd, cadena, sizeof(cadena));
+        close(fd);
+
+        // empieza a recibir el mensaje del server
+        fd2 = open("FIFO2", O_RDONLY);
+        // lee lo que le manda el servidor
+        read(fd2, cadena2, sizeof(cadena2));
+        // imprime lo que pasa
+        printf("\nServidor dice:  %s ", cadena2);
+        close(fd2);
+        fflush(stdin);
+
+        for (i = 0; i < 100; i++)
+        {
+            // reiniciar la cadena a 0
+            cadena[i] = '\0';
+            cadena2[i] = '\0';
+        }
+
+        fflush(stdin);
+        sleep(10);
+        menu_clientes();
+    } while (1);
+}
+
+void modifyCliente()
+{
+    char id_cliente[10];
+    char cadena[100];
+    char cadena2[100];
+
+    char nombre[50];
+    char apellido[50];
+    char rfc[50];
+    char direccion[50];
+    char edad[10];
+    char pais[50];
+    char telefono[11];
+    int opcion;
+    do
+    {
+        // se envia el mensaje.
+        fflush(stdin);
+        fd = open("FIFO1", O_WRONLY);
+        printf(" Que id de Cliente quieres editar:\n ");
+        scanf("%s", &id_cliente);
+        do
+        {
+            printf("\n|----------------|");
+            printf("\n| editar cliente |");
+            printf("\n|----------------|");
+            printf("\n| 1. Nombre      |");
+            printf("\n| 2. Apellidos   |");
+            printf("\n| 3. RFC         |");
+            printf("\n| 4. Direccion   |");
+            printf("\n| 5. Edad        |");
+            printf("\n| 6. telefono    |");
+            printf("\n| 7. regresar    |");
+            printf("\n|----------------|");
+            printf("\n\n Escoja una opcion: ");
+            scanf("%d", &opcion);
+
+            switch (opcion)
+            {
+            case 1:
+                printf("el nuevo nombre es:\n");
+                scanf("%s", &nombre);
+
+                sprintf(cadena, "UPDATE clientes SET nombre = '%s' WHERE id_cliente = %s", nombre, id_cliente);
+                // mandamos la sentencia sql
+                write(fd, cadena, sizeof(cadena));
+                close(fd);
+
+                // empieza a recibir el mensaje del server
+                fd2 = open("FIFO2", O_RDONLY);
+                // lee lo que le manda el servidor
+                read(fd2, cadena2, sizeof(cadena2));
+                // imprime lo que pasa
+                printf("\nServidor dice:  %s ", cadena2);
+                close(fd2);
+                fflush(stdin);
+
+                for (i = 0; i < 100; i++)
+                {
+                    // reiniciar la cadena a 0
+                    cadena[i] = '\0';
+                    cadena2[i] = '\0';
+                }
+
+                fflush(stdin);
+                sleep(10);
+                menu_clientes();
+                break;
+
+            case 2:
+                printf("\n Escribe el Apellido  :\n");
+                scanf("%s", &apellido);
+
+                sprintf(cadena, "UPDATE clientes SET apellidos = '%s' WHERE id_cliente = %s", apellido, id_cliente);
+                // mandamos la sentencia sql
+                write(fd, cadena, sizeof(cadena));
+                close(fd);
+
+                // empieza a recibir el mensaje del server
+                fd2 = open("FIFO2", O_RDONLY);
+                // lee lo que le manda el servidor
+                read(fd2, cadena2, sizeof(cadena2));
+                // imprime lo que pasa
+                printf("\nServidor dice:  %s ", cadena2);
+                close(fd2);
+                fflush(stdin);
+
+                for (i = 0; i < 100; i++)
+                {
+                    // reiniciar la cadena a 0
+                    cadena[i] = '\0';
+                    cadena2[i] = '\0';
+                }
+
+                fflush(stdin);
+                menu_clientes();
+                break;
+
+            case 3:
+                printf("\n Escribe el RFC :\n");
+                scanf("%s", &rfc);
+
+                sprintf(cadena, "UPDATE clientes SET rfc = '%s' WHERE id_cliente = %s", rfc, id_cliente);
+                // mandamos la sentencia sql
+                write(fd, cadena, sizeof(cadena));
+                close(fd);
+
+                // empieza a recibir el mensaje del server
+                fd2 = open("FIFO2", O_RDONLY);
+                // lee lo que le manda el servidor
+                read(fd2, cadena2, sizeof(cadena2));
+                // imprime lo que pasa
+                printf("\nServidor dice:  %s ", cadena2);
+                close(fd2);
+                fflush(stdin);
+
+                for (i = 0; i < 100; i++)
+                {
+                    // reiniciar la cadena a 0
+                    cadena[i] = '\0';
+                    cadena2[i] = '\0';
+                }
+
+                fflush(stdin);
+                menu_clientes();
+                break;
+
+            case 4:
+                printf("\n Escribe la nueva Direccion :\n");
+                scanf("%s", &direccion);
+
+                sprintf(cadena, "UPDATE clientes SET direccion = '%s' WHERE id_cliente = %s", direccion, id_cliente);
+                // mandamos la sentencia sql
+                write(fd, cadena, sizeof(cadena));
+                close(fd);
+
+                // empieza a recibir el mensaje del server
+                fd2 = open("FIFO2", O_RDONLY);
+                // lee lo que le manda el servidor
+                read(fd2, cadena2, sizeof(cadena2));
+                // imprime lo que pasa
+                printf("\nServidor dice:  %s ", cadena2);
+                close(fd2);
+                fflush(stdin);
+
+                for (i = 0; i < 100; i++)
+                {
+                    // reiniciar la cadena a 0
+                    cadena[i] = '\0';
+                    cadena2[i] = '\0';
+                }
+
+                fflush(stdin);
+
+                menu_clientes();
+                break;
+
+            case 5:
+                printf("\n Escribe la nueva edad  :\n");
+                scanf("%s", &edad);
+
+                sprintf(cadena, "UPDATE clientes SET edad = '%s' WHERE id_cliente = %s", edad, id_cliente);
+                // mandamos la sentencia sql
+                write(fd, cadena, sizeof(cadena));
+                close(fd);
+
+                // empieza a recibir el mensaje del server
+                fd2 = open("FIFO2", O_RDONLY);
+                // lee lo que le manda el servidor
+                read(fd2, cadena2, sizeof(cadena2));
+                // imprime lo que pasa
+                printf("\nServidor dice:  %s ", cadena2);
+                close(fd2);
+                fflush(stdin);
+
+                for (i = 0; i < 100; i++)
+                {
+                    // reiniciar la cadena a 0
+                    cadena[i] = '\0';
+                    cadena2[i] = '\0';
+                }
+
+                fflush(stdin);
+
+                menu_clientes();
+                break;
+
+            case 6:
+                printf("\n Escribe el Telefono nuevo  :\n");
+                scanf("%s", &telefono);
+
+                sprintf(cadena, "UPDATE clientes SET telefono = '%s' WHERE id_cliente = %s", telefono, id_cliente);
+                // mandamos la sentencia sql
+                write(fd, cadena, sizeof(cadena));
+                close(fd);
+
+                // empieza a recibir el mensaje del server
+                fd2 = open("FIFO2", O_RDONLY);
+                // lee lo que le manda el servidor
+                read(fd2, cadena2, sizeof(cadena2));
+                // imprime lo que pasa
+                printf("\nServidor dice:  %s ", cadena2);
+                close(fd2);
+                fflush(stdin);
+
+                for (i = 0; i < 100; i++)
+                {
+                    // reiniciar la cadena a 0
+                    cadena[i] = '\0';
+                    cadena2[i] = '\0';
+                }
+
+                fflush(stdin);
+
+                menu_clientes();
+                break;
+            case 7:
+                menu_clientes();
+                break;
+            default:
+                printf("\nOpcion no disponible\n");
+                break;
+            }
+        } while (opcion != 7);
+
+    } while (1);
+}
+
+void menu_facturas()
+{
     int opcion = 0;
-    do {
+    do
+    {
         printf("\n|----------------------------------------------|");
         printf("\n|             * Submenu Facturas *             |");
         printf("\n|----------------------------------------------|");
@@ -214,31 +507,34 @@ void menu_facturas() {
         printf("\n\n Escoja una opcion: ");
         scanf("%d", &opcion);
 
-        switch(opcion) {
-            case 1:
-                printf("Insertar facturas");
-                break;
-            case 2:
-                printf("Actualizar facturas");                       
-                break;
-            case 3:
-                printf("Eliminacion facturas");          
-                break;
-             case 4:
-                printf("regresando");          
-                inicio ();
-                break;
+        switch (opcion)
+        {
+        case 1:
+            printf("Insertar facturas");
+            break;
+        case 2:
+            printf("Actualizar facturas");
+            break;
+        case 3:
+            printf("Eliminacion facturas");
+            break;
+        case 4:
+            printf("regresando");
+            inicio();
+            break;
 
-            default:
-                printf("\nOpcion no disponible\n");     break;
+        default:
+            printf("\nOpcion no disponible\n");
+            break;
         }
-    }while (opcion != 4);
+    } while (opcion != 4);
 }
 
-
-void menu_articulos() {
+void menu_articulos()
+{
     int opcion = 0;
-    do {
+    do
+    {
         printf("\n|----------------------------------------------|");
         printf("\n|             * Submenu articulos *             |");
         printf("\n|----------------------------------------------|");
@@ -249,23 +545,25 @@ void menu_articulos() {
         printf("\n\n Escoja una opcion: ");
         scanf("%d", &opcion);
 
-        switch(opcion) {
-            case 1:
-                printf("insertar articulos");
-                break;
-            case 2:
-                printf("consultar articulos");                       
-                break;
-            case 3:
-                printf("Eliminar articulos");          
-                break;
-             case 4:
-                printf("regresando");          
-                inicio ();
-                break;
+        switch (opcion)
+        {
+        case 1:
+            printf("insertar articulos");
+            break;
+        case 2:
+            printf("consultar articulos");
+            break;
+        case 3:
+            printf("Eliminar articulos");
+            break;
+        case 4:
+            printf("regresando");
+            inicio();
+            break;
 
-            default:
-                printf("\nOpcion no disponible\n");     break;
+        default:
+            printf("\nOpcion no disponible\n");
+            break;
         }
-    }while (opcion != 4);
+    } while (opcion != 4);
 }
